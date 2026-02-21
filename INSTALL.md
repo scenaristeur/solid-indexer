f9becf59153997e80ef1ba5f6bbbbd92ce77ab86
scenaristeur <scenaristeur@gmail.com>
2026-02-21 12:20:04 +0100

    install

--- INSTALL.md
+++ INSTALL.md
@@ -21,11 +21,10 @@ ansible-playbook ./INSTALL/1-install.yml --ask-become-pass -v
 ./INSTALL/initialisation.sh
 ```
 
-Token identifier: dummy_token_71f3105e-8a1a-4018-bea6-4fb251720ecd
-Token secret: 07d04c47e2d0c2619aac30b80383cc7dbd5fc5eb9f1c184b87ae7f7cecddd2908a77e93d6514181767ab9d2fbfc4b175e09109a150cc2e615a26dd53c81578ff
-
 - create a Solid Account, create a Solid Pod, create a TOKEN_IDENTIFIER and a TOKEN_SECRET and paste them in DATA_FOLDER=$HOME/.solid-indexer
 - [initialisation-walkthrough](INSTALL/initialisation.html)
+- [register](http://localhost:3000/.account/login/password/register/)
+- copy & paste TOKEN_IDENTIFIER & TOKEN_SECRET in $HOME/.solid-indexer/.env
 
 # Start and enjoy !
 
--- INSTALL/initialisation.sh
+++ INSTALL/initialisation.sh
@@ -13,11 +13,11 @@ echo "Solid community Server is running in another terminal window"
 echo "Wait for Solid Community Server to be ready, when it says 'Listening to server at http://localhost:3000/'"
 echo -e "\nIn the next step, you will register a ACCOUNT, create a POD to store your data, and create a TOKEN to allow apps to act on your POD and store it in ${ENV_FILE}"
 echo -e "Two new html pages ansd a new terminal will open :\n\t- http://localhost:3000/.account/login/password/register/ to register your account.\n\t- ./INSTALLATION/initialisation.html to guide you through the process"
-echo "copy TOKEN_IDENTIFIER and TOKEN_SECRET and past THEM in ${ENV_FILE}/"
+echo "copy TOKEN_IDENTIFIER and TOKEN_SECRET and past them in ${ENV_FILE}/"
 
 
 read -p "Press enter to continue"
 open "http://localhost:3000/.account/login/password/register/" &
 open ./INSTALL/initialisation.html &
 
-gnome-terminal -- sh -c "bash -c \"\"${EDITOR:-vi}\" file.txt; exec bash\""
+gnome-terminal -- sh -c "bash -c \"\"${EDITOR:-vi}\" ${ENV_FILE}; exec bash\""
